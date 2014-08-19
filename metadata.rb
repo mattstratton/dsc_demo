69d5966be2e4ca0aded42ee0f4e1b32ce28e9890
Matt Stratton <matt.stratton@gmail.com>
2014-08-19 18:06:48 -0500

    Remove redundant serverspec tests

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures dsc_demo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends "windows", "~> 1.31.0"
 depends 'dsc'
--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -2,6 +2,4 @@ require 'spec_helper'
 
 describe windows_feature('DSC-Service') do
     it{ should be_installed }
-    it{ should be_installed.by("dism") }
-    it{ should be_installed.by("powershell") }
 end
